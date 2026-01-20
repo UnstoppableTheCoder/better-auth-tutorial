3488f89a710bb637b84156a9254d4068f5154595
vipendra <vipendrasinghpatel999@gmail.com>
2026-01-20 18:23:00 +0530

    change baseURL

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -5,7 +5,8 @@ import { auth } from "./auth";
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://better-auth-tutorial-eb9k.vercel.app",
   // nextCookies() - mainly needed when working with server actions - Know more about it
   // inferAdditionalFields() -> I added extra role field so I need to infer the types of the user on the client side too
   plugins: [inferAdditionalFields<typeof auth>(), nextCookies()],
