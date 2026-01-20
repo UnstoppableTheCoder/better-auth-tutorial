bbd05e2e37903c32afc98721239d2e6180d8ffa1
vipendra <vipendrasinghpatel999@gmail.com>
2026-01-20 19:14:04 +0530

    added

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,7 +4,7 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import { auth } from "./auth";
 
 export const authClient = createAuthClient({
-  baseURL: "https://better-auth-tutorial-eb9k.vercel.app/",
+  baseURL: "https://better-auth-tutorial-ten.vercel.app/",
   // nextCookies() - mainly needed when working with server actions - Know more about it
   // inferAdditionalFields() -> I added extra role field so I need to infer the types of the user on the client side too
   plugins: [inferAdditionalFields<typeof auth>(), nextCookies()],
